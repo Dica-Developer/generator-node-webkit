385bdaf3b4598e6da73e081c6b9a8f5f3b758c83
Martin Schaaf <mschaaf@datameer.com>
2014-02-21 11:31:14 +0100

    * format

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ var tar = require('tar-fs');
 var fs = require('fs');
 var zlib = require('zlib');
 
+/*jshint camelcase: false*/
 var proxy = process.env.http_proxy || process.env.HTTP_PROXY || process.env.https_proxy || process.env.HTTPS_PROXY || null;
 var githubOptions = {
   version: '3.0.0'
@@ -30,11 +31,13 @@ var suggestAppNameFromPath = function (_) {
   return _.slugify(basePath);
 };
 
-var NodeWebkitGenerator = module.exports = function NodeWebkitGenerator(args, options, config) {
+var NodeWebkitGenerator = module.exports = function NodeWebkitGenerator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+    this.installDependencies({
+      skipInstall: options['skip-install']
+    });
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
@@ -50,50 +53,40 @@ NodeWebkitGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [
-    {
-      name: 'appName',
-      message: 'What do you want to call your app?',
-      default: appName
-    },
-    {
-      name: 'appDescription',
-      message: 'A little description for your app?'
-    },
-    {
-      name: 'githubUser',
-      message: 'Would you mind telling me your username on GitHub?',
-      default: 'someuser'
-    },
-    {
-      type: 'checkbox',
-      name: 'platforms',
-      message: 'Which platform do you wanna support?',
-      choices: [
-        {
-          name: 'MacOS',
-          checked: true
-        },
-        {
-          name: 'Linux 64',
-          checked: true
-        },
-        {
-          name: 'Windows',
-          checked: true
-        },
-        {
-          name: 'Linux 32'
-        },
-      ],
-      validate: function( answer ) {
-        if ( answer.length < 1 ) {
-          return "You must choose at least one platform.";
-        }
-        return true;
+  var prompts = [{
+    name: 'appName',
+    message: 'What do you want to call your app?',
+    default: appName
+  }, {
+    name: 'appDescription',
+    message: 'A little description for your app?'
+  }, {
+    name: 'githubUser',
+    message: 'Would you mind telling me your username on GitHub?',
+    default: 'someuser'
+  }, {
+    type: 'checkbox',
+    name: 'platforms',
+    message: 'Which platform do you wanna support?',
+    choices: [{
+      name: 'MacOS',
+      checked: true
+    }, {
+      name: 'Linux 64',
+      checked: true
+    }, {
+      name: 'Windows',
+      checked: true
+    }, {
+      name: 'Linux 32'
+    }, ],
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one platform.';
       }
+      return true;
     }
-  ];
+  }];
 
   this.prompt(prompts, function (props) {
     var _this = this;
@@ -107,20 +100,20 @@ NodeWebkitGenerator.prototype.askFor = function askFor() {
     this.Windows = false;
     this.github = false;
 
-    props.platforms.forEach(function(platform){
-      switch(platform){
-        case 'MacOS':
-          _this.MacOS = true;
-          break;
-        case 'Linux 32':
-          _this.Linux32 = true;
-          break;
-        case 'Linux 64':
-          _this.Linux64 = true;
-          break;
-        case 'Windows':
-          _this.Windows = true;
-          break;
+    props.platforms.forEach(function (platform) {
+      switch (platform) {
+      case 'MacOS':
+        _this.MacOS = true;
+        break;
+      case 'Linux 32':
+        _this.Linux32 = true;
+        break;
+      case 'Linux 64':
+        _this.Linux64 = true;
+        break;
+      case 'Windows':
+        _this.Windows = true;
+        break;
       }
     });
     done();
@@ -144,10 +137,12 @@ NodeWebkitGenerator.prototype.userInfo = function userInfo() {
     }
   };
 
-  if(this.githubUser !== 'someuser'){
+  if (this.githubUser !== 'someuser') {
     this.log.info('Get GitHub informations');
     this.github = true;
-    github.user.getFrom({ user: this.githubUser }, responseClbk);
+    github.user.getFrom({
+      user: this.githubUser
+    }, responseClbk);
   } else {
     done();
   }
@@ -157,107 +152,106 @@ NodeWebkitGenerator.prototype.app = function app() {
   this.mkdir('app');
   this.mkdir('app/img');
   this.mkdir('resources/node-webkit');
-  if(this.MacOS){
+  if (this.MacOS) {
     this.mkdir('resources/node-webkit/MacOS');
   }
-  if(this.Linux32){
+  if (this.Linux32) {
     this.mkdir('resources/node-webkit/Linux32');
   }
-  if(this.Linux64){
+  if (this.Linux64) {
     this.mkdir('resources/node-webkit/Linux64');
   }
-  if(this.Windows){
+  if (this.Windows) {
     this.mkdir('resources/node-webkit/Windows');
   }
   this.mkdir('tmp');
 };
 
-NodeWebkitGenerator.prototype.getNodeWebkit = function getNodeWebkit(){
+NodeWebkitGenerator.prototype.getNodeWebkit = function getNodeWebkit() {
   var done = this.async();
-  var successClbk = function(){
+  var successClbk = function () {
     done();
   };
-  var failureClbk = function(error){
+  var failureClbk = function (error) {
     throw error;
   };
   when.all(this._getNodeWebkit(), successClbk, failureClbk);
 };
 
-
-NodeWebkitGenerator.prototype._getNodeWebkit = function _getNodeWebkit(){
+NodeWebkitGenerator.prototype._getNodeWebkit = function _getNodeWebkit() {
   var promises = [];
-  if(this.MacOS){
+  if (this.MacOS) {
     promises.push(this._requestNodeWebkit('osx-ia32', '.zip', 'MacOS'));
   }
-  if(this.Linux32){
+  if (this.Linux32) {
     promises.push(this._requestNodeWebkit('linux-ia32', '.tar.gz', 'Linux32'));
   }
-  if(this.Linux64){
+  if (this.Linux64) {
     promises.push(this._requestNodeWebkit('linux-x64', '.tar.gz', 'Linux64'));
   }
-  if(this.Windows){
+  if (this.Windows) {
     promises.push(this._requestNodeWebkit('win-ia32', '.zip', 'Windows'));
   }
   return promises;
 };
 
-NodeWebkitGenerator.prototype._requestNodeWebkit = function _requestNodeWebkit(versionString, extension, platform){
+NodeWebkitGenerator.prototype._requestNodeWebkit = function _requestNodeWebkit(versionString, extension, platform) {
   var defer = when.defer(),
     _this = this,
     contentType = extension === '.zip' ? 'application/zip' : 'application/x-tar';
-  if (!fs.existsSync('tmp/'+ platform + extension)) {
+  if (!fs.existsSync('tmp/' + platform + extension)) {
     this.log.info('Downloading node-webkit for ' + platform);
     https.get(this.nodeWebkitBaseUrl + versionString + extension, function (res) {
       console.log(res.headers['content-type']);
-      if(res.headers['content-type'] === contentType){
+      if (res.headers['content-type'] === contentType) {
         res.on('data', function (chunk) {
-          fs.appendFileSync('tmp/'+ platform + extension, chunk);
+          fs.appendFileSync('tmp/' + platform + extension, chunk);
         }).on('end', function () {
-            _this.log.ok('Node-webkit for ' + platform + ' downloaded');
-            defer.resolve();
-        }).on('error', function(error){
-            _this.log.conflict('Error while downloading node-webkit for ' + platform, error);
-            defer.reject(error);
+          _this.log.ok('Node-webkit for ' + platform + ' downloaded');
+          defer.resolve();
+        }).on('error', function (error) {
+          _this.log.conflict('Error while downloading node-webkit for ' + platform, error);
+          defer.reject(error);
         });
       } else {
         _this.log.conflict('Wrong content type for %s', platform);
         defer.reject('Wrong content type for ' + platform);
       }
-    }).on('error', function(error){
-        _this.log.conflict('Error while downloading node-webkit for ' + platform, error);
-        defer.reject(error);
+    }).on('error', function (error) {
+      _this.log.conflict('Error while downloading node-webkit for ' + platform, error);
+      defer.reject(error);
     });
-  } else{
+  } else {
     this.log.ok('Node-webkit for ' + platform + ' already downloaded');
     defer.resolve();
   }
   return defer.promise;
 };
 
-NodeWebkitGenerator.prototype.unzipNodeWebkit = function unzipNodeWebkit(){
+NodeWebkitGenerator.prototype.unzipNodeWebkit = function unzipNodeWebkit() {
   var done = this.async();
-  var successClbk = function(){
+  var successClbk = function () {
     done();
   };
-  var failureClbk = function(error){
+  var failureClbk = function (error) {
     throw error;
   };
   when.all(this._unzipNodeWebkit(), successClbk, failureClbk);
 };
 
-NodeWebkitGenerator.prototype._unzipNodeWebkit = function _unzipNodeWebkit(){
+NodeWebkitGenerator.prototype._unzipNodeWebkit = function _unzipNodeWebkit() {
   var promises = [];
 
-  if(this.MacOS){
+  if (this.MacOS) {
     promises.push(this._extract('MacOS', '.zip'));
   }
-  if(this.Linux32){
+  if (this.Linux32) {
     promises.push(this._extract('Linux32', '.tar.gz'));
   }
-  if(this.Linux64){
+  if (this.Linux64) {
     promises.push(this._extract('Linux64', '.tar.gz'));
   }
-  if(this.Windows){
+  if (this.Windows) {
     promises.push(this._extract('Windows', '.zip'));
   }
   return promises;
@@ -300,44 +294,44 @@ NodeWebkitGenerator.prototype._extract = function _extract(platform, extension) 
 NodeWebkitGenerator.prototype._copyNodeWebkit = function _copyNodeWebkit(platform) {
   var _this = this;
   this.log.info('Copy %s files.', platform);
-  switch(platform){
-    case 'MacOS':
-      fs.copy('tmp/mac/node-webkit.app', 'resources/node-webkit/mac/node-webkit.app', function (error) {
-        if (error) {
-          _this.log.conflict('Error while copying files for '+ platform +'!', error);
-        } else {
-          _this.log.ok('%s files successfully copied.', platform);
-//          _this._cleanUp(platform);
-        }
-      });
-      break;
-    default:
-      this.log.ok('No files to copy');
+  switch (platform) {
+  case 'MacOS':
+    fs.copy('tmp/mac/node-webkit.app', 'resources/node-webkit/mac/node-webkit.app', function (error) {
+      if (error) {
+        _this.log.conflict('Error while copying files for ' + platform + '!', error);
+      } else {
+        _this.log.ok('%s files successfully copied.', platform);
+        //          _this._cleanUp(platform);
+      }
+    });
+    break;
+  default:
+    this.log.ok('No files to copy');
   }
 };
 
 NodeWebkitGenerator.prototype._cleanUp = function _cleanUp(platform) {
   var _this = this;
   this.log.info('Cleaning up tmp directories for %s.', platform);
-  switch(platform){
-    case 'MacOS':
-      fs.remove('tmp/mac', function(error){
-        if(error){
-          _this.log.conflict('Error while deleting tmp directory for '+ platform +'!', error);
-        } else {
-          _this.log.ok('Tmp directory for %s successfully removed.', platform);
-        }
-      });
-      fs.remove('tmp/node-webkit-osx-ia32.zip', function(error){
-        if(error){
-          _this.log.conflict('Error while deleting zip file for '+ platform +'!', error);
-        } else {
-          _this.log.ok('Zip file for %s successfully removed.', platform);
-        }
-      });
-      break;
-    default:
-      this.log.ok('No files to copy');
+  switch (platform) {
+  case 'MacOS':
+    fs.remove('tmp/mac', function (error) {
+      if (error) {
+        _this.log.conflict('Error while deleting tmp directory for ' + platform + '!', error);
+      } else {
+        _this.log.ok('Tmp directory for %s successfully removed.', platform);
+      }
+    });
+    fs.remove('tmp/node-webkit-osx-ia32.zip', function (error) {
+      if (error) {
+        _this.log.conflict('Error while deleting zip file for ' + platform + '!', error);
+      } else {
+        _this.log.ok('Zip file for %s successfully removed.', platform);
+      }
+    });
+    break;
+  default:
+    this.log.ok('No files to copy');
   }
 };
 
@@ -357,4 +351,3 @@ NodeWebkitGenerator.prototype.copyFiles = function copyFiles() {
   this.template('mac-files/_Info.plist.tmp', 'resources/mac-files/Info.plist.tmp');
   this.template('mac-files/_Info.plist', 'resources/mac-files/Info.plist');
 };
-
