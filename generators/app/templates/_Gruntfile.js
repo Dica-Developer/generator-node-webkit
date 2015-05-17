99a9fb4f52cf47728630f3a8e8f63aee2d207167
Jörg Weber <weber.joerg.h@gmail.com>
2015-05-17 22:16:16 +0200

    Add basic Gruntfile as template

--- generators/app/templates/_Gruntfile.js
+++ generators/app/templates/_Gruntfile.js
@@ -1,3 +1,20 @@
-/**
- * Created by jweber on 17/05/15.
- */
+module.exports = function (grunt) {
+  'use strict';
+
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    paths: {
+      'nwjsSource': 'nwjs',
+      'tmp': 'tmp',
+      'app': 'app',
+      'dist': 'dist',
+      'resources': 'resources'
+    },
+    clean: {},
+    copy: {}
+  });
+
+  grunt.loadTasks('./grunt-tasks');
+};
