38319f75d33e8fea3cfea95ccea1f7849351e13e
Jörg Weber <weber.joerg.h@gmail.com>
2015-05-26 07:21:26 +0200

    Add a task to Gruntfile template to easily change the version of the app

--- generators/app/templates/_Gruntfile.js
+++ generators/app/templates/_Gruntfile.js
@@ -18,5 +18,13 @@ module.exports = function (grunt) {
     rename: {}
   });
 
+  grunt.registerTask('bump', 'bump app version', function (type) {
+    var packageContent = grunt.config.get('pkg');
+    packageContent.version = semver.inc(packageContent.version, type || 'patch');
+    grunt.file.write('package.json', JSON.stringify(packageContent, null, 2));
+    grunt.config.set('pkg', grunt.file.readJSON('package.json'));
+    grunt.log.ok('Version bumped to ' + packageContent.version);
+  });
+
   grunt.loadTasks('./grunt-tasks');
 };
